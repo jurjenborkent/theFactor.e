60eaf87faf064b383356b1ba73ddb05ba574ecdc
Jeramai Faber <jeramaiqfaber@hotmail.com>
2020-01-22 12:55:42 +0100

    - Added login screen

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+require('bootstrap');
+
 if (environment.production) {
   enableProdMode();
 }
